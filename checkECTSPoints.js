3c1d3a568038c4d6bf92b7455f9b1d42f55bf592
Devulak <Devulak@users.noreply.github.com>
2018-06-13 13:02:39 +0200

    The code's a mess, it's only a hack job

--- checkECTSPoints.js
+++ checkECTSPoints.js
@@ -10,9 +10,15 @@ function CheckECTSPoints()
 		var resultTable = document.getElementById("resultTable");
 		var tBody = resultTable.getElementsByTagName("tbody")[0];
 		var tResult = tBody.getElementsByTagName("tr");
+		var total;
 
 		for (var i = 0; i < tResult.length; i++)
 		{
+			if(tResult[i].id == "Total")
+			{
+				total = tResult[i];
+				continue;
+			}
 			var values = this.getCorrectValues(tResult[i]);
 			this.classes.push(values); // put into classes
 
@@ -20,14 +26,50 @@ function CheckECTSPoints()
 			{
 				this.totalECTSPoints += Number(values['ETCS']);
 				this.classesPassed.push(values);
+				tResult[i].style = "background-color: #57bb8a;";
+			}
+			else
+			{
+				tResult[i].style = "background-color: #e67c73;";
 			}
 		}
+		if(!total)
+		{
+			var total = tBody.lastElementChild.cloneNode(true);
+			total.id = "Total";
+			total.style = "";
+			tBody.appendChild(total);
+		}
+		totalChildren = total.children;
+		for (var i = 0; i < totalChildren.length; i++)
+		{
+			totalChildren[i].innerHTML = "";
+		}
+		totalChildren[totalChildren.length - 1].innerHTML = this.totalECTSPoints.toFixed(1);
+
+
+		var totalWeight = 0;
+		var totalGrade = 0;
+
+		var totalUnweighted = 0;
+		var totalUnweightedGrade = 0;
+		for (var i = 0; i < this.classesPassed.length; i++)
+		{
+			if(!isNaN(this.classesPassed[i]['grades']))
+			{
+				totalWeight += Number(this.classesPassed[i]['ETCS']);
+				totalGrade += Number(this.classesPassed[i]['ETCS'] * this.classesPassed[i]['grades']);
+				totalUnweighted++;
+				totalUnweightedGrade += Number(this.classesPassed[i]['grades']);
+			}
+		}
+		totalChildren[totalChildren.length - 3].innerHTML = (totalGrade / totalWeight).toFixed(1) + "\n\r(Unweighted: " + (totalUnweightedGrade / totalUnweighted).toFixed(1) + ")";
 	}
 
 	this.passed = function(values)
 	{
 		var passed = false;
-		if (values['gradesScale'].includes("F") || values['grades'].includes("I"))
+		if (values['gradesScale'].includes("F") || values['grades'].includes("AV") || values['grades'].includes("I"))
 		{
 			passed = false;
 		}
@@ -52,38 +94,8 @@ function CheckECTSPoints()
 		return correctValues;
 	}
 
-	this.getClasses = function()
-	{
-		console.log("================================");
-		for (var i = this.classes.length - 1; i >= 0; i--)
-		{
-			var values = this.classes[i];
-			var passed = this.passed(values);
-			console.log("Checking " + values['name'] + " (" + values['code'] + ") ... " + (passed ? "PASSED" : "FAILED") + " ... " + values['ETCS'] + " ETCS");
-		}
-	}
-
-	this.getClassesPassed = function()
-	{
-		console.log("================================");
-		for (var i = 0; i < this.classesPassed.length; i++)
-		{
-			var values = this.classesPassed[i];
-			console.log(values['name'] + " (" + values['code'] + ") ... " + values['ETCS'] + " ETCS");
-		}
-	}
-
-	this.getTotalPassedECTS = function()
-	{
-		console.log("================================");
-		console.log(this.totalECTSPoints);
-	}
-
 	this.fetchClasses();
 }
 
 // Edit as you like
 var checker = new CheckECTSPoints();
-checker.getClasses();
-checker.getClassesPassed();
-checker.getTotalPassedECTS();
